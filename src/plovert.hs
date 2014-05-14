e340afe9fc46ab0ff8a56b3760a9b911d1cef1fe
Calvin Beck <hobbes@ualberta.ca>
2014-05-14 08:04:45 -0600

    Bit more done. Added a dictionary structure (not sure if I am going to use this). Made sure we could split the strokes in the dictionary, and added a stenoOrder string.

--- src/plovert.hs
+++ src/plovert.hs
@@ -1,10 +1,29 @@
 import Data.Aeson
-import Data.ByteString.Lazy hiding (head)
-import Data.Map
+import Data.ByteString.Lazy hiding (head, map)
+import Data.Map hiding (map)
 import System.Environment
 import Prelude hiding (readFile, take)
+import Data.Binary
+import Data.List.Split
+import Control.Applicative
+
+data DictEntry = DictEntry { dicStroke :: Word32
+                           , dicFlags :: Word32
+                           , dicParent :: Word32
+                           , dicLeft :: Word32
+                           , dicRight :: Word32
+                           , dicRoot :: Word32
+                           , dicLength :: Word32
+                           , dicEntry :: ByteString
+                           , dicNext :: Word32
+                           }
+
+stenoOrder = "STKPWHRAO*EUFRPBLGTSDZ"
 
 main = do
   args <- getArgs
   dictionary <- readFile $ head args
-  print $ (eitherDecode dictionary :: Maybe String (Map String String))
+  print $ dictSplit <$> (eitherDecode dictionary :: Either String (Map String String))
+
+dictSplit :: Map String String -> [[String]]
+dictSplit dict = map (splitOn "/") $ keys dict
